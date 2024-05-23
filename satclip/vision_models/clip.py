94753972bbf1ddecc77ef3751c3567a1030639a1
Aayush <aayush.dhakal224@gmail.com>
2024-05-23 19:16:54 +0000

    added CLIP vision encoder

---
+++ satclip/vision_models/clip.py
@@ -0,0 +1,27 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from transformers import CLIPImageProcessor,CLIPVisionModelWithProjection, CLIPVisionConfig
+from torchvision.transforms import RandAugment
+import pytorch_lightning as pl
+import imageio
+import sys
+from argparse import Namespace
+import code
+
+class Clip(pl.LightningModule):
+    def __init__(self, device, vit_type='16'):
+        super().__init__()
+        
+        self.vit_map = {'32':'openai/clip-vit-base-patch32', '16':'openai/clip-vit-base-patch16', '14L':'openai/clip-vit-large-patch14'}
+        # print(f'Args.vit is {args.vit}')
+        # self.vit = self.vit_map[args.vit]
+        self.vision_model = CLIPVisionModelWithProjection.from_pretrained(self.vit_map[vit_type]).train()
+
+    def forward(self,x):
+        batch_tensors = self.vision_model(x)
+        unnormalized_batch_embeddings = batch_tensors.image_embeds
+        return unnormalized_batch_embeddings
+
+if __name__ == '__main__':
+    model = Clip('cuda', '16')
