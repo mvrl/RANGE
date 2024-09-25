cf27cc78321ea4b4b5732e3e0c1d9e72a0f2dafd
Aayush Dhakal <aayush.dhakal224@gmail.com>
2024-09-24 22:46:42 -0500

    added option to load both location and image models

--- satclip/vision_models/satmae.py
+++ satclip/vision_models/satmae.py
@@ -21,6 +21,23 @@ class SatMAE(nn.Module):
         output = self.projection_layer(pooled_tensors)
         return output
 
+class SatMAE_Raw(nn.Module):
+    def __init__(self, device='cuda'):
+        super().__init__()
+        
+        self.vision_model = Satmae.from_pretrained("MVRL/satmae-vitlarge-fmow-pretrain-800")
+        # del self.vision_model.decoder_embed
+        # del self.vision_model.mask_token
+        # del self.vision_model.decoder_pos_embed
+        # del self.vision_model.decoder_blocks
+        # del self.vision_model.decoder_norm
+        # del self.vision_model.decoder_pred
+
+    def forward(self,x):
+        batch_tensors = self.vision_model.forward_encoder(x, mask_ratio=0.0)[0]
+        pooled_tensors = batch_tensors[:,1:,:].mean(dim=1)
+        return pooled_tensors
+
 if __name__ == '__main__':
     model = SatMAE(256, 'cuda')
     data = torch.rand(2,3,224,224)
